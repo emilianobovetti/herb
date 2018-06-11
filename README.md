174231c405cbab362671223bc7f577fa2505fd57
Emiliano Bovetti <emiliano.bovetti@gmail.com>
2018-06-11 16:53:29 +0200

    style: format js

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-herb
-====
+# herb
 
 ![all-products-0](https://user-images.githubusercontent.com/3957026/40659148-af9a50f2-634d-11e8-94f4-07acdc1276d5.png)
 
--- electron.js
+++ electron.js
@@ -4,7 +4,7 @@ const path = require('path');
 const url = require('url');
 const fs = require('fs');
 
-//const settingsPath = path.join(app.getPath('userData'), 'settings.json');
+// const settingsPath = path.join(app.getPath('userData'), 'settings.json');
 const settingsPath = 'settings.json';
 const indexPath = path.join(__dirname, 'web', 'index.html');
 
@@ -13,7 +13,7 @@ fs.openSync(settingsPath, 'r+');
 const settings = JSON.parse(fs.readFileSync(settingsPath));
 
 if (settings.debug === undefined) {
-    settings.debug = settings.environment === 'development';
+  settings.debug = settings.environment === 'development';
 }
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -21,32 +21,32 @@ if (settings.debug === undefined) {
 let win;
 
 const createWindow = function () {
-    // Create the browser window.
-    win = new BrowserWindow(settings.windowOptions);
+  // Create the browser window.
+  win = new BrowserWindow(settings.windowOptions);
 
-    // pass settings to renderer processes
-    win.settings = settings;
+  // pass settings to renderer processes
+  win.settings = settings;
 
-    // and load the index.html of the app.
-    win.loadURL(url.format({
-        pathname: indexPath,
-        protocol: 'file:',
-        slashes: true
-    }));
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: indexPath,
+    protocol: 'file:',
+    slashes: true
+  }));
 
-    if (settings.environment === 'development') {
-        // Open the DevTools.
-        win.webContents.openDevTools();
-    }
+  if (settings.environment === 'development') {
+    // Open the DevTools.
+    win.webContents.openDevTools();
+  }
 
-    // Emitted when the window is closed.
-    win.on('closed', () => {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
-        win = null;
-    });
-};
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null;
+  });
+}
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -55,17 +55,17 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow();
-    }
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow();
+  }
 });
